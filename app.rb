bab48772f13fd95ee9d693d2b5e4f7f4e8a4bda6
victor <victor.buthia@student.moringaschool.com>
2023-02-06 11:18:59 +0300

    completed

--- app.rb
+++ app.rb
@@ -4,9 +4,10 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+
+ puts "Hello World!"
+ print "Pass this test, please."
+ p [1,2,3]
 
 # def add
     
@@ -14,10 +15,3 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
